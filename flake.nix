d4d347d2685e164aeb09cdaac8a58ce16d235423
Sebastian Grzybowski <Sebastian Grzybowski>
2025-12-02 14:44:54 +0100

    v0

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    alejandra = {
+      url = "github:kamadorueda/alejandra/4.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    alejandra,
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        {
+          environment.systemPackages = [alejandra.defaultPackage.x86_64-linux];
+        }
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
