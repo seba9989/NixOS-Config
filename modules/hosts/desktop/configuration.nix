b34bb0363bf1ac789e2cd7cffe906727a50b5949
Seba9989 <sgrzybbowski@gmail.com>
2026-03-29 13:20:08 +0200

    Add Desktop

---
+++ modules/hosts/desktop/configuration.nix
@@ -0,0 +1,58 @@
+{self, ...}: {
+  flake.nixosModules.DesktopConfiguration = {pkgs, ...}: {
+    networking.hostName = "Desktop";
+    # import any other modules from here
+    imports = [
+      self.nixosModules.DesktopHardware
+      self.nixosModules.niri
+    ];
+
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+
+    users.users.seba9989 = {
+      isNormalUser = true;
+      description = "Seba9989";
+      extraGroups = ["networkmanager" "wheel"];
+
+      shell = pkgs.fish;
+    };
+
+    nixpkgs.config.allowUnfree = true;
+    environment.systemPackages = with pkgs; [
+      firefox
+      vscode
+      nixd
+      alejandra
+      jujutsu
+      git
+      gh
+    ];
+
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+      flake = "/home/seba9989/.config/NixOS-Config"; # sets NH_OS_FLAKE variable for you
+    };
+
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting
+      '';
+    };
+
+    # ...
+
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    # services.displayManager.gdm.enable = true;
+    services.displayManager.enable = true;
+    systemd.services.lidm.enable = true;
+    services.desktopManager.gnome.enable = true;
+
+    system.stateVersion = "26.05";
+  };
+}
